1f98009f0102cb502a79f4661d71077212b7bf0d
mokhope <mokopesurprise8@gmail.com>
2025-05-13 12:37:38 +0200

    added functionality to get the correct time

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function updateTime() {
   // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
@@ -29,9 +28,6 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
